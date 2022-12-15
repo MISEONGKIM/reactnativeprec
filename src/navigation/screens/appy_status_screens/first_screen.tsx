66e75311b007c81e8f15cdfc80a26381e14d49f2
MISEONGKIM <rlaaltjd46@jinhakapply.com>
2022-12-15 11:58:27 +0900

    feat: link 추가

--- src/navigation/screens/appy_status_screens/first_screen.tsx
+++ src/navigation/screens/appy_status_screens/first_screen.tsx
@@ -2,7 +2,7 @@ import {View, Text, Button} from 'react-native';
 import React from 'react';
 import {SearchView} from '@components/search_view';
 import {SwitchView} from '@components';
-import {useNavigation} from '@react-navigation/native';
+import {Link, useNavigation} from '@react-navigation/native';
 import {TopTabScreenProps} from '@navigation/type';
 
 const FirstScreen = () => {
@@ -12,9 +12,10 @@ const FirstScreen = () => {
       <SearchView />
       <SwitchView text="첫번째 시험" />
       <Button
-        onPress={() => screenProps.navigation.navigate('Ticket')}
-        title="go Ticket"
+        onPress={() => screenProps.navigation.navigate('Second')}
+        title="go Main"
       />
+      <Link to={{screen: 'Ticket'}}>Ticket Link</Link>
     </View>
   );
 };
